9573490fb1e9b2d9771a55e9bb1516b0844d310b
nikhil kumar <nikhilkumar02102005@gmail.com>
2024-08-23 14:17:29 +0530

    Datatypes and their conversions

---
+++ conversion.js
@@ -0,0 +1,35 @@
+let score = true
+let x = null
+
+console.log(typeof score)
+console.log(typeof (score)) //method
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let xInNum = Number(x)  //converting to nnumber
+console.log(typeof x)
+console.log(typeof xInNum)
+console.log(xInNum)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+
+let isLoggedIn = "nk"
+let booleanIsLoggedIn = Boolean(isLoggedIn) //converting to boolean
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false (empty string)
+// "nikhil" => true
+
+
+let num = 33
+ 
+let stringNum = String(num) //converting to string
+console.log(stringNum)
+console.log(typeof stringNum)
---
+++ dataTypes.js
@@ -0,0 +1,9 @@
+"use strict" //treat all code as newer version
+
+// alert(3+3)      
+//we uing nodejs not the browser
+
+console.log(typeof "Nikhil")
+console.log(typeof 23)
+console.log(typeof undefined)   //undefined
+console.log(typeof null)    //object
