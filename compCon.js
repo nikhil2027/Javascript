40602cb80c547db69f203d31942777d59ac82ce0
nikhil kumar <nikhilkumar02102005@gmail.com>
2024-08-25 10:34:30 +0530

    comparison

---
+++ compCon.js
@@ -0,0 +1,8 @@
+console.log(null > 0);
+console.log(null == 0);     //this gives false because only comparison operator converts null to zero
+console.log(null >= 0);     //comparison operator convert null to zero and then compare 
+
+console.log("2" == 2);   //this only check the value not its datatype, it converts to same datatype and then converts
+
+console.log("2" === 2);     // === checks the value as well as datatype 
+console.log(3 === 3);
