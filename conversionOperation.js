789da2dd93108391927922292e80af06550ab422
nikhil kumar <nikhilkumar02102005@gmail.com>
2024-08-25 10:36:17 +0530

    string and something

---
+++ conversionOperation.js
@@ -0,0 +1,21 @@
+let someNumber = 33;
+let stringNumber = String(someNumber);
+/*
+console.log(stringNumber);
+console.log(typeof stringNumber);*/
+
+//***********************operations*****************************
+
+let value = 33;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = "Nikhil";
+let str2 = " Kumar";
+let str3 = str1 + str2;
+console.log(str3);
+
+
+console.log(true);
+console.log(+true);
+console.log(+"");
